dc10f7ad0b329f5062de2401b5e0f33b9426b9a3
Amy Martinsen <martinsen.styling@gmail.com>
2019-02-20 13:58:53 -0800

    added blackjack score method

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -1,7 +1,15 @@
 # blackjack_score.rb
 
-VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'King', 'Queen', 'Jack']
+VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "King", "Queen", "Jack"]
+FACE_CARDS = ["King", "Queen", "Jack"]
 
 def blackjack_score(hand)
+  unless hand.include?(VALID_CARDS)
+    raise ArgumentError, "Hand must include only valid cards."
+  end
 
+  score = hand.reduce(:+)
+  return score
 end
+
+# puts blackjack_score([3, 4, "Emperor"])
